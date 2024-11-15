333b16701976612cc75d9056453ddaff03e13bda
unknown <sanmoon@gmail.com>
2024-11-15 14:40:47 +0600

    JS-conditionals-tasks

---
+++ Free_Drinks.js
@@ -0,0 +1,15 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+
+let BurgerPrice = 500;
+
+if(BurgerPrice >= 500){
+    console.log("Your drink is free.");
+}else{
+    console.log("You need to pay 30tk.");
+}
